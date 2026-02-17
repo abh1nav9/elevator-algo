cdef20653b761cb14fde9305782ff2ced793b044
Abhinav Gautam <ABHINAVGAUTAM.210111063@gehu.ac.in>
2026-02-18 01:00:23 +0530

    method on pointer

--- main.go
+++ main.go
@@ -2,15 +2,34 @@ package main
 
 import "fmt"
 
+// struct
 type Elevator struct {
 	currentFloor int
 }
 
+// methon on pointer
+
+// Here we created a method which will get memory add for struct as pointer and then accessing the struct variables aka inheritence
+func (e *Elevator) MoveUp() {
+	e.currentFloor++
+}
+
+func (e *Elevator) MoveDown() {
+	e.currentFloor--
+}
+
 func main() {
 
-	e := Elevator{
-		currentFloor: 1,
-	}
+	e := Elevator{currentFloor: 1}
+	fmt.Println("Now at floor:", e.currentFloor)
 
-	fmt.Println("Elevator is at floor:", e.currentFloor)
+	e.MoveUp()
+	fmt.Println("Now at floor:", e.currentFloor)
+
+	e.MoveUp()
+	fmt.Println("Now at floor:", e.currentFloor)
+
+	e.MoveDown()
+	fmt.Println("Now at floor:", e.currentFloor)
+
 }
